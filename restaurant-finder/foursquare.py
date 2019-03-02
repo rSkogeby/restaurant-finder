937b3f2f0e58d69e35cd82de9253dd5fa5956c02
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-02 16:26:06 +0000

    Initial commit

---
+++ restaurant-finder/foursquare.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Return restaurant based on meal type and coordinates."""
+import requests
+import json
+
---
+++ restaurant-finder/web_interface.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""Serve up a webpage for finding a restaurant given a preferred meal type and location."""
+
+
