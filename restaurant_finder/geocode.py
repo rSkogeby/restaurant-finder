1dd410f36c2d57dec8362639971aa0a86dbe56ea
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-03 10:16:30 +0000

    Add google api call that returns coordinates

--- restaurant_finder/geocode.py
+++ restaurant_finder/geocode.py
@@ -3,3 +3,12 @@
 import requests
 import json
 
+from instance.config import getGoogleAPIKey
+
+def getCoordinates(location):
+    api_key = getGoogleAPIKey()
+    cleaned_location = location
+    api_call = 'https://maps.googleapis.com/maps/api/geocode/json?address={},&key={}'.format(cleaned_location,api_key)
+    data = requests.get(api_call)
+    coordinates=json.loads(data.content.decode())['results'][0]['geometry']['location']    
+    return coordinates
