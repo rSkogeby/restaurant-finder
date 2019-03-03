9378e0925c34d62bd03a69de1a436a0931522119
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-03 09:23:12 +0000

    Add landing page boilerplate

--- restaurant-finder/web_interface.py
+++ restaurant-finder/web_interface.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
 """Serve up a webpage for finding a restaurant given a preferred meal type and location."""
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET','POST'])
+def landingPage():
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        pass
+
 
