036eaf9dd9e6b7a91e89460878321295a2a6e8ff
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-03 09:55:04 +0000

    Rename directory to restaurant_finder

--- restaurant-finder/foursquare.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-"""Return restaurant based on meal type and coordinates."""
-import requests
-import json
-
--- restaurant-finder/geocode.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-"""Return longitude and latitute based on location."""
-import requests
-import json
-
