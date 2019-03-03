beca9a85ca5021758bef37ffcfafa2280d647bd3
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-03 10:20:29 +0000

    Add docstring for getCoordinates(location)

--- restaurant_finder/geocode.py
+++ restaurant_finder/geocode.py
@@ -5,7 +5,16 @@ import json
 
 from instance.config import getGoogleAPIKey
 
+
 def getCoordinates(location):
+    """Return dictionary containing the latitude and longitude.
+    
+    Input: location is the location of the desired coordinates. Formatted 
+    as if you were to type it into google maps.
+
+    Output: dictionary with keys 'lat' and 'lng' for returning the
+    latitude and longitude of the best match of the search string.
+    """
     api_key = getGoogleAPIKey()
     cleaned_location = location
     api_call = 'https://maps.googleapis.com/maps/api/geocode/json?address={},&key={}'.format(cleaned_location,api_key)
